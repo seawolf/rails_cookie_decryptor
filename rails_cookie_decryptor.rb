ff69d284093fba50143be68835d99b955d5b9637
Ben Arnold <ben@seawolfsanctuary.com>
2017-02-03 10:20:58 +0000

    Up: extract change events to class for mallet-wielding error handling

--- rails_cookie_decryptor.rb
+++ rails_cookie_decryptor.rb
@@ -7,6 +7,7 @@ end
 
 require 'base64'
 require 'json'
+require 'cgi'
 require 'active_support'
 
 module Decryptor
@@ -34,8 +35,8 @@ module Decryptor
 
   def self.rails_3(cookie_str)
     cookie_str = URI.unescape(cookie_str)
-    data, _digest = cookie_str.split('--')
-    return DEFAULT_MSG unless data.to_s.length
+    data, _digest = cookie_str.to_s.split('--')
+    return DEFAULT_MSG unless data.present?
 
     decoded_data = ::Base64.decode64(data.to_s)
     begin
@@ -45,13 +46,49 @@ module Decryptor
   end
 
   def self.rails_4(cookie_str, key, salt, salt_signed, key_iter_num)
-    return DEFAULT_MSG unless (cookie_str.to_s.length && key.to_s.length && salt.to_s.length && salt_signed.to_s.length && key_iter_num.to_i > 0)
+    return DEFAULT_MSG unless cookie_str.present? && key.present?
 
-    key_generator = ::ActiveSupport::KeyGenerator.new(key, iterations: key_iter_num)
-    secret = key_generator.generate_key(salt)
-    sign_secret = key_generator.generate_key(salt_signed)
+    begin
+      key_generator = ::ActiveSupport::KeyGenerator.new(key, iterations: key_iter_num.to_s.to_i)
+      secret = key_generator.generate_key(salt)
+      sign_secret = key_generator.generate_key(salt_signed)
+      encryptor = ::ActiveSupport::MessageEncryptor.new(secret, sign_secret)
+    rescue; end
+    return DEFAULT_MSG unless encryptor.present?
 
-    encryptor = ::ActiveSupport::MessageEncryptor.new(secret, sign_secret)
+    begin
+      cookie = CGI.unescape(cookie_str)
+      result = encryptor.decrypt_and_verify(cookie)
+    rescue; end
+    return result || DEFAULT_MSG
+  end
+end
+
+class AppLogic
+  def initialize(shoes_app)
+    %i{ cookie format output options key salt salt_signed key_iter_num }.each do |ivar|
+      ivar = "@#{ivar}"
+      instance_variable_set(ivar, shoes_app.instance_variable_get(ivar))
+    end
+  end
+
+  def toggle_options
+    if @format.text == Decryptor::AVAILABLE_FORMATS[:rails_2_or_3]
+      @options.hide
+    else
+      @options.show
+    end
+  end
+
+  def decrypt_cookie
+    data = Decryptor.from(@format.text, @cookie.text, @key.text, @salt.text, @salt_signed.text, @key_iter_num.text)
+    pretty_output = begin
+      JSON.pretty_generate(data)
+    rescue JSON::GeneratorError
+      data
+    end
+
+    @output.text = pretty_output
   end
 end
 
@@ -71,7 +108,7 @@ Shoes.app(title: APP_TITLE, height: 775) do
       @options = stack(hidden: true) do
         flow do
           para('Secret Key Base:', width: 200)
-          @key = edit_line('', width: 350, right: 0)
+          @key = edit_line('', width: 350, height: 50, right: 0, margin_bottom: 10)
         end
 
         flow do
@@ -96,32 +133,31 @@ Shoes.app(title: APP_TITLE, height: 775) do
       @output = edit_box('', width: 575, height: 350, state: 'readonly')
     end
 
-    @cookie.change do |_editbox|
-      data = Decryptor.from(@format.text, @cookie.text, @key.text, @salt.text, @salt_signed.text, @key_iter_num.text.to_s.to_i)
-      pretty_output = begin
-        JSON.pretty_generate(data)
-      rescue JSON::GeneratorError
-        data
-      end
+    app_logic = AppLogic.new(self)
 
-      @output.text = pretty_output
+    @cookie.change do |_editbox|
+      app_logic.decrypt_cookie
     end
 
     @format.change do |_editbox|
-      if @format.text == Decryptor::AVAILABLE_FORMATS[:rails_2_or_3]
-        @options.hide
-      else
-        @options.show
-      end
+      app_logic.toggle_options
+      app_logic.decrypt_cookie
+    end
 
-      data = Decryptor.from(@format.text, @cookie.text, @key.text, @salt.text, @salt_signed.text, @key_iter_num.text.to_s.to_i)
-      pretty_output = begin
-        JSON.pretty_generate(data)
-      rescue JSON::GeneratorError
-        data
-      end
+    @key.change do |_editline|
+      app_logic.decrypt_cookie
+    end
 
-      @output.text = pretty_output
+    @salt.change do |_editline|
+      app_logic.decrypt_cookie
+    end
+
+    @salt_signed.change do |_editline|
+      app_logic.decrypt_cookie
+    end
+
+    @key_iter_num.change do |_editline|
+      app_logic.decrypt_cookie
     end
   end
 end
